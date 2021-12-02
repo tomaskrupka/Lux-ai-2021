4d26754af928143b6a1eb2c8aea2e73f06321f92
Tomas Krupka <tomas@tomaskrupka.eu>
2021-12-02 16:27:39 +0000

    Simplify positions_scores at step within resource. No functional change.

--- bot_orchestrated/develop_cluster_actions.py
+++ bot_orchestrated/develop_cluster_actions.py
@@ -283,6 +283,7 @@ def step_within_resources(units_on_resource, cluster, cluster_development_settin
                     if min_dist_to_export > dist:
                         min_dist_to_export = dist
                 min_dist_to_empty = 100
+                min_dist_to_export_penalty = min_dist_to_export * 10
                 for position in mineable_development_positions:
                     # Add position next to unlocked resources only for loaded units
                     can_mine_on_position = cluster_extensions.can_mine_on_position(
@@ -293,8 +294,7 @@ def step_within_resources(units_on_resource, cluster, cluster_development_settin
                         dist = position.distance_to(target)
                         if dist < min_dist_to_empty:
                             min_dist_to_empty = dist
-                positions_scores[target] = 10000 - min_dist_to_export * 1000 - min_dist_to_empty - 100 * (
-                        cluster.cell_infos[target].my_city_tile is not None)
+                positions_scores[target] = 100 - min_dist_to_export_penalty - min_dist_to_empty
 
             positions_options = []
             for position in units_on_resource:
